c2ef35a60d146055603acd4966cbb8f88b29027c
TommyCakes <tomhwarhurst@gmail.com>
2015-07-14 23:52:00 +0100

    Created portfolio

---
+++ app.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+	
+
+	// vars
+	var width = 720;
+	var animationSpeed = 1000;
+	var pause = 3000;
+	var currentSlide = 1;
+
+	//cache DOM
+	var $slider = $('#slider');
+	var $slideContainer = $slider.find('.slides');
+	var $slides = $slideContainer.find('.slide');
+
+	var interval;
+
+	function startSlider(){
+		interval = setInterval(function() {
+		$slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function(){
+			currentSlide++;
+			if (currentSlide === $slides.length){
+				currentSlide = 1;
+				$slideContainer.css('margin-left', 0);
+			}
+		});
+		}, pause);
+	}
+
+	function stopSlider(){
+		clearInterval(interval);
+	}
+
+	$slider.on('mouseenter', stopSlider).on('mouseleave',  startSlider);
+
+	startSlider();
+
+	// $('.int')on('click', function(){
+	// 	$('.int').addClass('animated.shake');
+	// }):
+
+	//listen for mouseenter and pause
+	//resume on mouse leave
+
+});
